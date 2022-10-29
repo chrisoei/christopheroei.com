fc3bdaaa9c99dcada91248fae23843918a2cd5dc
Chris Oei <chris.oei@gmail.com>
2022-10-29 09:05:50 -0700

    Add common.ts (a library of functions for use across the project)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import { blobURL } from "./common"
 
 const App = () => {
   return (
     <div className="App">
-        Please pardon the dust -- I'll put up real content soon.
+          Please pardon the dust -- I'll put up real content soon.
+          <ul>
+              <li>
+                  <a href={blobURL({
+                      sha2_256: "bbfae219246fa05f2e4df0181ca892157e39752ca80ecf6c1992d18c2c42e899",
+                      ext: "pdf"
+                  })}>Turing Undecidability and the Efficient Market Hypothesis</a>
+
+              </li>
+          </ul>
     </div>
   );
 }
---
+++ src/common.ts
@@ -0,0 +1,23 @@
+﻿export interface Blob {
+    sha2_256: string
+    ext: string
+}
+
+export const baseURL = "https://www.christopheroei.com/"
+export const blobBaseURL = baseURL + "b/"
+
+export const blobURL = (b: Blob): string => {
+    return blobBaseURL + b.sha2_256 + "." + b.ext
+}
+
+export const getBlob = (u: string): Blob => {
+    var r1 = /(\p{Hex_Digit}{64})\.([0-9A-Za-z]+)$/u
+    var m1 = u.match(r1)
+    console.assert(m1 != null)
+    if (m1 == null) {
+        // FIXIT
+        return { sha2_256: "error", ext: "error" }
+    } else {
+        return { sha2_256: m1[1], ext: m1[2] }
+    }
+}
